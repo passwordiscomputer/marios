817a2f500e50f2f4bd7872980b07976fa5d64bf4
Guest User <Guest@Epicodus-4D.local>
2018-04-20 15:16:04 -0700

    add add new product spec

---
+++ spec/features/add_product_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "the add a Product process" do
+  it "adds a new product" do
+    visit products_path
+    click_link 'New Product'
+    fill_in 'Name', :with => 'Home stuff'
+    fill_in 'Cost', :with => 20
+    fill_in 'Country', :with => 'USA'
+    click_on 'Create Product'
+    expect(page).to have_content 'Products'
+  end
+end
