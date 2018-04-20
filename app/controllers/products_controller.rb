2950a68a7986bcbbf1ee8518f13e79c0580baeab
Guest User <Guest@Epicodus-4D.local>
2018-04-20 13:56:56 -0700

    add edit update new and create methods for products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,26 @@ class ProductsController < ApplicationController
     render :show
   end
 
+  def new
+    @product = Product.new
+    render :new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to  products_path
+    else
+      render :new
+    end
+  end
+
+
+  def edit
+    @product = Product.find(params[:id])
+    render :edit
+  end
+
   def update
     @product= product.find(params[:id])
     if @product.update(product_params)
@@ -26,6 +46,6 @@ class ProductsController < ApplicationController
 end
 
 private
- def list_params
+ def product_params
    params.require(:product).permit(:name, :cost, :country)
  end
