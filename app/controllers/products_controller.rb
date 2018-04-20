45137827eb28e731bcdf643bc0e74cf79b48229d
Guest User <Guest@Epicodus-4D.local>
2018-04-20 13:47:01 -0700

    add update and destroy methods to product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,4 +9,18 @@ class ProductsController < ApplicationController
     @reviews = @product.reviews
     render :show
   end
+
+  def update
+    @list= List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to lists_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Product.find(params[:id]).destroy
+    redirect_to products_path
+  end
 end
