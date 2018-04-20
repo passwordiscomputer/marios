b0e6ac9342ccf957999863b7fc6e2193870a9e53
Guest User <Guest@Epicodus-4D.local>
2018-04-20 10:12:01 -0700

    add validation test for association with product

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -8,4 +8,5 @@ describe Review do
   it { should validate_presence_of :content_body}
   it { should validate_length_of(:content_body).is_at_least(50)}
   it { should validate_length_of(:content_body).is_at_most(250)}
+  it { should belong_to(:product) }
 end
